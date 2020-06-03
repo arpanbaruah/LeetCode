b056ffd74c44c6b011fba1735a3ff2bded340b40
Arpan Baruah <arpanbaruah.83@gmail.com>
2020-06-03 01:10:02 -0700

    760

---
+++ GoProject/P760FindAnagram.go
@@ -0,0 +1,17 @@
+func anagramMappings(A []int, B []int) []int {
+    
+    mp := make(map[int][]int)
+    for i, v := range B {
+        mp[v] = append(mp[v], i)
+    }
+    
+    res := make([]int , len(B))
+    
+    for i, v := range A {
+        temp := mp[v]
+        res[i] = temp[0]
+        temp = temp[1:]
+    }
+    
+    return res
+}
